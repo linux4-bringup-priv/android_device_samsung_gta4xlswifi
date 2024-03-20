e0265db4e25ec187953afc09b53a50422e4a9dc3
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    gta4xlwifi: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- lineage_gta4xlwifi.mk
+++ lineage_gta4xlwifi.mk
@@ -29,9 +29,6 @@ TARGET_SCREEN_WIDTH := 1200
 ## Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
-## Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 ## Device identifier, this must come after all inclusions
 PRODUCT_DEVICE := gta4xlwifi
 PRODUCT_NAME := lineage_gta4xlwifi
